711d46636f620fd30779f54e3075eb0bd59f68b5
Alex Schofield <git@ajschof.me>
2026-04-27 19:31:47 +0100

    add user-agent header to request in get_latest_data()

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ SORT_KV = {
     "distance": "distance",
 }
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/533.45 (KHTML, like Gecko) Chrome/48.0.2094.221 Safari/602"
+}
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -40,7 +44,7 @@ def get_location(address: str) -> tuple[float, float]:
 
 def get_latest_data() -> Tuple[pd.DataFrame, str]:
     try:
-        response = requests.get(ENDPOINT)
+        response = requests.get(ENDPOINT, headers=HEADERS, timeout=10)
         response.raise_for_status()
     except Exception as e:
         raise e
