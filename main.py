86fa52ee1e896ed8eb8942fcecfee30a95125be9
Alex Schofield <git@ajschof.me>
2026-04-27 22:02:40 +0100

    fix typo in raise statement in get_location()

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_location(address: str) -> tuple[float, float]:
     geolocator = Nominatim(user_agent="FuelNearMe")
     result = geolocator.geocode(address)
     if not isinstance(result, Location):
-        raise ValueError(f"Failed to get location from address: '{address}")
+        raise ValueError(f"Failed to get location from address: '{address}'")
     return (result.latitude, result.longitude)
 
 
