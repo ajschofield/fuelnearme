5f8766e73475fffe173ad1c3d9f59a1e8eebd8dc
Alex Schofield <git@ajschof.me>
2026-06-19 08:57:50 +0100

    remove DATABASE_URL env arg from connect_db()

--- fnme/db/connection.py
+++ fnme/db/connection.py
@@ -3,7 +3,6 @@ import os
 import sqlalchemy as sql
 
 
-def connect_db():
-    DATABASE_URL = os.environ["DATABASE_URL"]
-    engine = sql.create_engine(DATABASE_URL, echo=False)
+def connect_db(URL):
+    engine = sql.create_engine(URL, echo=False)
     return engine
